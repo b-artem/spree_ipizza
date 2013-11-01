1866db88983bc9db18f134689f6d0ec032410307
Dmitri Zutskov <keeruline@gmail.com>
2013-11-01 23:31:55 +0200

    making routes rails 4 ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
   namespace :gateway do
-    match '/banklink/success'   => 'ipizza#success', :as => :ipizza_success, :method => :post
-    match '/banklink/failure'   => 'ipizza#failure', :as => :ipizza_failure, :method => :post
-    match '/banklink/:order_id' => 'ipizza#show',    :as => :ipizza, :constraints => 'R(\d{9})$'
+    post '/banklink/success'   => 'ipizza#success', :as => :ipizza_success
+    post '/banklink/failure'   => 'ipizza#failure', :as => :ipizza_failure
+    get  '/banklink/:order_id' => 'ipizza#show',    :as => :ipizza, :constraints => 'R(\d{9})$'
   end
 end
