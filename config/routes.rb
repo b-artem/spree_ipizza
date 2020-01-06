d96899cab1a1f870626555fc795590a071b2a258
Artem Barylchenko <artemb.dev@gmail.com>
2020-01-06 15:24:14 +0200

    Fix constraints for ipizza#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Spree::Core::Engine.routes.draw do
   namespace :gateway do
     post '/banklink/success'   => 'ipizza#success', :as => :ipizza_success
     post '/banklink/failure'   => 'ipizza#failure', :as => :ipizza_failure
-    get  '/banklink/:order_id' => 'ipizza#show',    :as => :ipizza, :constraints => 'R(\d{9})$'
+    get  '/banklink/:order_id' => 'ipizza#show',    :as => :ipizza, constraints: { id: /\d{9}\z/ }
   end
 end
